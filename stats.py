1a8d3bf3360764bfe05ad1c72642de309df1b805
luigifaticoso <faticosoluigi@gmail.com>
2019-03-22 11:57:55 +0100

    stats update

--- stats.py
+++ stats.py
@@ -2,6 +2,15 @@ import os
 
 path = os.path.abspath("preprocessed_files")
 dirs = os.listdir( path )
+count = 0
+average_sentence_duration = 0
+duration = []
 for i in dirs:
     if i.split(".")[1] == "PHN":
-        print(i)
+        f = open(os.path.join(path,i),"r")
+        for i in f.readlines():
+            start_time = int(i.split(" ")[0])
+            end_time = int(i.split(" ")[1])
+            phoneme = i.split(" ")[2]
+            duration.append(end_time-start_time)
+print(sum(duration)/len(duration))
